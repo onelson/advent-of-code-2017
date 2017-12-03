3c007c075fa339a33d134b5716c069562c6cb67b
Owen Nelson <onelson@gmail.com>
2017-12-02 20:43:44 -0800

    gold star for day 2 part 1!

--- src/bin/day02a.rs
+++ src/bin/day02a.rs
@@ -4,4 +4,14 @@ use std::env;
 use aoc::checksum;
 
 
-fn main() {}
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let fp = &args[1];
+    println!("reading spreadsheet: {}", fp);
+    let data = checksum::read_file(fp);
+    let cells = checksum::parse_cells(&data);
+
+    let result = checksum::calculate(&cells);
+
+    println!("{}", result);
+}
--- src/checksum.rs
+++ src/checksum.rs
@@ -17,3 +17,44 @@
 //! The second row's largest and smallest values are 7 and 3, and their difference is 4.
 //! The third row's difference is 6.
 //! In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
+
+
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
+
+pub fn read_file<P: AsRef<Path>>(path: P) -> String {
+    let mut file = File::open(path).unwrap();
+    let mut buf = String::new();
+    let _ = file.read_to_string(&mut buf).unwrap();
+    buf
+}
+
+pub fn parse_cells(input: &str) -> Vec<Vec<u32>> {
+    input.lines().map(|line| {
+        line.split_whitespace().map(|x| x.parse::<u32>().unwrap()).collect()
+    }).collect()
+}
+
+pub fn calculate(input: &[Vec<u32>]) -> u32 {
+    input.iter().fold(0, |acc, row| {
+        acc + (row.iter().max().unwrap() - row.iter().min().unwrap())
+    })
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        let input = vec![
+            vec![5, 1, 9, 5],
+            vec![7, 5, 3],
+            vec![2, 4, 6, 8]
+        ];
+
+        assert_eq!(calculate(&input), 18);
+    }
+}
