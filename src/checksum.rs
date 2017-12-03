eca0c4b0534cc0ca66f08c3aedfe873770d2f891
Owen Nelson <onelson@gmail.com>
2017-12-02 21:03:34 -0800

    add the 2nd part of each brief

--- src/checksum.rs
+++ src/checksum.rs
@@ -17,6 +17,29 @@
 //! The second row's largest and smallest values are 7 and 3, and their difference is 4.
 //! The third row's difference is 6.
 //! In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
+//!
+//! --- Part Two ---
+//!
+//! "Great work; looks like we're on the right track after all. Here's a star for your effort."
+//! However, the program seems a little worried. Can programs be worried?
+//!
+//! "Based on what we're seeing, it looks like all the User wanted is some information about the
+//! evenly divisible values in the spreadsheet. Unfortunately, none of us are equipped for that
+//! kind of calculation - most of us specialize in bitwise operations."
+//! It sounds like the goal is to find the only two numbers in each row where one evenly divides
+//! the other - that is, where the result of the division operation is a whole number. They would
+//! like you to find those numbers on each line, divide them, and add up each line's result.
+//! For example, given the following spreadsheet:
+//!
+//! 5 9 2 8
+//! 9 4 7 3
+//! 3 8 6 5
+//!
+//! In the first row, the only two numbers that evenly divide are 8 and 2; the result of this
+//! division is 4.
+//! In the second row, the two numbers are 9 and 3; the result is 3.
+//! In the third row, the result is 2.
+//! In this example, the sum of the results would be 4 + 3 + 2 = 9.
 
 
 use std::path::Path;
@@ -31,9 +54,14 @@ pub fn read_file<P: AsRef<Path>>(path: P) -> String {
 }
 
 pub fn parse_cells(input: &str) -> Vec<Vec<u32>> {
-    input.lines().map(|line| {
-        line.split_whitespace().map(|x| x.parse::<u32>().unwrap()).collect()
-    }).collect()
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect()
+        })
+        .collect()
 }
 
 pub fn calculate(input: &[Vec<u32>]) -> u32 {
@@ -49,11 +77,7 @@ mod tests {
 
     #[test]
     fn test_example() {
-        let input = vec![
-            vec![5, 1, 9, 5],
-            vec![7, 5, 3],
-            vec![2, 4, 6, 8]
-        ];
+        let input = vec![vec![5, 1, 9, 5], vec![7, 5, 3], vec![2, 4, 6, 8]];
 
         assert_eq!(calculate(&input), 18);
     }
