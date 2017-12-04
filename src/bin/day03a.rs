95a034b4fdf3c87ac970813ef00d0619dc22ac93
Owen Nelson <onelson@gmail.com>
2017-12-04 01:43:43 -0800

    fmt and add brief for day3 pt 2

--- src/bin/day03a.rs
+++ src/bin/day03a.rs
@@ -4,4 +4,10 @@ use std::env;
 use aoc::spiral_memory;
 
 
-fn main() {}
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let input = &args[1];
+    println!("walking to cell: {}", input);
+    let result = spiral_memory::walk(input.parse::<u64>().unwrap());
+    println!("{}", result);
+}
--- src/spiral_memory.rs
+++ src/spiral_memory.rs
@@ -26,15 +26,49 @@
 //!
 //! How many steps are required to carry the data from the square identified in your puzzle input
 //! all the way to the access port?
+//!
+//! --- Part Two ---
+//!
+//! As a stress test on the system, the programs here clear the grid and then store the value 1 in
+//! square 1. Then, in the same allocation order as shown above, they store the sum of the values
+//! in all adjacent squares, including diagonals.
+//!
+//! So, the first few squares' values are chosen as follows:
+//!
+//! Square 1 starts with the value 1.
+//! Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+//! Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+//! Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their
+//! values, 4.
+//! Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+//! Once a square is written, its value does not change. Therefore, the first few squares would
+//! receive the following values:
+//!
+//! 147  142  133  122   59
+//! 304    5    4    2   57
+//! 330   10    1    1   54
+//! 351   11   23   25   26
+//! 362  747  806--->   ...
+//! What is the first value written that is larger than your puzzle input?
 
 #[derive(Copy, Clone, Debug)]
-enum Direction { North, South, East, West }
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
 
 #[derive(Copy, Clone, Debug)]
-struct Pos { x: i64, y: i64 }
+struct Pos {
+    x: i64,
+    y: i64,
+}
 
 impl Pos {
-    fn zero() -> Pos { Pos { x: 0, y: 0 } }
+    fn zero() -> Pos {
+        Pos { x: 0, y: 0 }
+    }
 }
 
 #[derive(Debug)]
@@ -43,7 +77,7 @@ struct Walker {
     value: u64,
     /// top left, bottom right
     inner_bounds: (Pos, Pos),
-    heading: Direction
+    heading: Direction,
 }
 
 impl Walker {
@@ -52,7 +86,7 @@ impl Walker {
             pos: Pos::zero(),
             value: 1,
             inner_bounds: (Pos::zero(), Pos::zero()),
-            heading: Direction::East
+            heading: Direction::East,
         }
     }
 }
@@ -63,7 +97,6 @@ impl Iterator for Walker {
 
     fn next(&mut self) -> Option<Self::Item> {
         let item = match self.heading {
-
 
             Direction::East => {
                 if self.inner_bounds.1.x < self.pos.x {
@@ -125,8 +158,9 @@ impl Iterator for Walker {
 }
 
 pub fn walk(n: u64) -> u64 {
-    if n < 2 { 0 }
-    else {
+    if n < 2 {
+        0
+    } else {
         let (_, pos) = Walker::new().find(|v| v.0 == n).unwrap();
         println!("{} = {:?}", n, pos);
         (pos.x.abs() + pos.y.abs()) as u64
