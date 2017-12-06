c26ab9ef0ec30b8ed7badfe20e58d310d6f6b1d8
Owen Nelson <onelson@gmail.com>
2017-12-05 22:11:45 -0800

    gold star for day 5 pt 1

--- src/bin/day05a.rs
+++ src/bin/day05a.rs
@@ -4,4 +4,12 @@ use std::env;
 use aoc::trampoline_maze;
 
 
-fn main() {}
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+    let fp = &args[1];
+    println!("reading instruction set: {}", fp);
+    let data = aoc::read_file(fp);
+    let mut instructions = trampoline_maze::parse_instructions(&data);
+    let result = trampoline_maze::execute(&mut instructions);
+    println!("{}", result);
+}
--- src/trampoline_maze.rs
+++ src/trampoline_maze.rs
@@ -35,3 +35,70 @@
 //!
 //! How many steps does it take to reach the exit?
 //!
+
+pub fn parse_instructions(data: &str) -> Vec<i64> {
+    data.lines().map(|x| x.parse::<i64>().unwrap()).collect()
+}
+
+
+/// updates the value of the cell at `idx` returns the updated position
+fn jump(idx: usize, xs: &mut [i64]) -> Option<usize> {
+    // calculate where we will land first
+    let dest = (xs[idx] + idx as i64) as usize;
+    xs[idx] += 1;
+    // check to see if the dest is out of bounds or not
+    if dest < xs.len() { Some(dest) } else { None }
+}
+
+pub fn execute(instructions: &mut [i64]) -> u64 {
+    let mut destination = jump(0, instructions);
+
+    // only increment the moves if we have somewhere to go?
+    let mut moves = if destination.is_some() { 1 } else { 0 };
+
+    while let Some(idx) = destination {
+        moves += 1;
+        destination = jump(idx, instructions);
+    }
+
+    moves
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_by_jump() {
+        let mut instructions = vec![0, 3, 0, 1, -3];
+        let idx = Some(0);
+        let idx = jump(idx.unwrap(), &mut instructions);
+        assert_eq!(idx, Some(0));
+        assert_eq!(instructions, vec![1, 3, 0, 1, -3]);
+
+        let idx = jump(idx.unwrap(), &mut instructions);
+        assert_eq!(idx, Some(1));
+        assert_eq!(instructions, vec![2, 3, 0, 1, -3]);
+
+        let idx = jump(idx.unwrap(), &mut instructions);
+        assert_eq!(idx, Some(4));
+        assert_eq!(instructions, vec![2, 4, 0, 1, -3]);
+
+
+        let idx = jump(idx.unwrap(), &mut instructions);
+        assert_eq!(idx, Some(1));
+        assert_eq!(instructions, vec![2, 4, 0, 1, -2]);
+
+
+        let idx = jump(idx.unwrap(), &mut instructions);
+        assert_eq!(idx, None);
+        assert_eq!(instructions, vec![2, 5, 0, 1, -2]);
+    }
+
+    #[test]
+    fn test_example_by_execute() {
+        let mut instructions = vec![0, 3, 0, 1, -3];
+        assert_eq!(execute(&mut instructions), 5);
+    }
+
+}
